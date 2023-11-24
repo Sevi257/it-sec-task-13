4c80c0a3faceacde2d7da12cf7c3f06dfac3f467
leitseve <sevi1@web.de>
2023-11-24 15:32:50 +0100

    locaal

--- pwn-students.py
+++ pwn-students.py
@@ -7,29 +7,20 @@ import numpy
 # Name of the cookie
 COOKIE = "session"
 MAC_SIZE = 4
-def mh5(x, mac):
+def mh5(x):
     state = 0
 
-    # Apply padding
-    #x = x + b"\x80"  # Terminate message with 0x80
-    # 2 Zeichen waren noch frei aber eines davon ist 0x80
-    # Es wird echt viel dann wenn man appended
-    # Man kann nur Bytes appenden aber die müssen den richtigen Wert haben -> wie kann man das ausrechnen
-    # Mit einem for loop über valid bytes drübergehen und wenn es dann a
-    # Bis auf einen Block auffüllen dann bytes.fromHex(Differenz) dann anhängen
-    # Zusammenhang mit Secret Key
-    print("Padding: ", (MAC_SIZE - (len(x) % MAC_SIZE)))
-    x = x
+    x = x + b"\x80"
     x = x + (MAC_SIZE - (len(x) % MAC_SIZE)) * b"\x00"
     # Split into chunks
-    for i in range(0, len(x), MAC_SIZE):
-        state += int.from_bytes(x[i:i + MAC_SIZE], byteorder="big")
+    for j in range(0, len(x), MAC_SIZE):
+        state += int.from_bytes(x[j:j + MAC_SIZE], byteorder="big")
         state &= (2 ** 32 - 1)
     return state.to_bytes(length=MAC_SIZE, byteorder="big")
 
 
-#url = "https://t13.itsec.sec.in.tum.de/950357d650d4fa78"
-url = "http://127.0.0.1:5000"
+url = "https://t13.itsec.sec.in.tum.de/950357d650d4fa78"
+#url = "http://127.0.0.1:5000"
 
 with requests.Session() as session:
     r = session.get(url)
@@ -48,16 +39,25 @@ with requests.Session() as session:
     # mh5(secretkey + sessiondata) = mac
     # '{"u": "admin", }'
     mac = original_cookie[:8]
-    print(mac)
+    print("MAC: ", mac)
     jsondump = original_cookie[8:]
 
-    test = '{"u": "admin", "t": "Hel2ë|$lo"}'.encode()
-    mac_p = mh5(test, mac)
-    print(mac_p.hex())
-    testtest = json.loads(bytes.fromhex(test.hex()))
-    print(testtest)
-    test =  b"1111aaaa" + test
-    session.cookies.set(name=COOKIE, value=test.hex(), domain="http://127.0.0.1:5000")
+    i = 0
+    while True:
+        test_data = ('{\"u\": \"admin\", \"extra_data\": "' + str(i) + '"\"}').encode()
+        test_mac = mh5(test_data)
+        #print("Testmac: ", test_mac)
+        if test_mac.hex() == mac:
+            print(f"Collision found! i={i}, test_data={test_data}")
+            break
+        if i == 65536:
+            print("50%")
+        if i == 2147483648:
+            print("Unlikely")
+        i += 1
+
+    testtest = json.loads(bytes.fromhex(test_data.hex()))
+    session.cookies.set(name=COOKIE, value=test_data.hex(), domain="https://t13.itsec.sec.in.tum.de/9")
     print(f'Values: {session.cookies.values()} and Keys: {session.cookies.keys()}')
     r = session.get(url)
     print(r.text)
